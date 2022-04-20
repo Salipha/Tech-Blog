df636a205110111c88de089280a1add1e49fed03
Salipha <92833183+Salipha@users.noreply.github.com>
2022-04-20 17:24:27 -0400

    update

---
+++ api/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes)
+
+module.exports = router;
